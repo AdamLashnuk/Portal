9650efee1b1e731a2b8f4274777434698798c94c
Adam <lashnukadam@gmail.com>
2026-07-04 00:38:51 -0400

    added one singular commnet on line 1616

--- app/chat_panel.py
+++ app/chat_panel.py
@@ -1613,6 +1613,7 @@ class ChatPanel(QWidget):
                 self.drag_position = event.globalPosition().toPoint() - self.frameGeometry().topLeft()
                 event.accept()
 
+    # Resize the window based on the dragged edge or corner.
     def mouseMoveEvent(self, event):
         position = event.position().toPoint()
 
