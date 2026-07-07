76c215e50b19b5bdd21f748d03c578ec60731e26
ffarisx5 <felembanfaris5@gmail.com>
2026-07-07 13:49:31 +0300

    Added Z.ai to the list of llms.

--- app/chat_panel.py
+++ app/chat_panel.py
@@ -89,7 +89,8 @@ class AddLLMDialog(QDialog):
             {"name": "Gemini", "url": "https://gemini.google.com"},
             {"name": "Perplexity", "url": "https://perplexity.ai"},
             {"name": "DeepSeek", "url": "https://chat.deepseek.com"},
-            {"name": "HuggingFace", "url": "https://huggingface.co/chat/"}
+            {"name": "HuggingFace", "url": "https://huggingface.co/chat/"},
+            {"name": "Z.ai", "url": "https://z.ai/"}
         ]
         self.populate_list(self.all_llms)
 
